d4a72dc216d140639d095f91d435af98f03ccfd0
centr <centras51@gmail.com>
2024-10-21 08:06:48 +0300

    perkeliamos funkcijos iš anonymous į ui_helpers

--- c51_library_system/ui/ui_helpers.py
+++ c51_library_system/ui/ui_helpers.py
@@ -3,9 +3,11 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 
-BACKGROUND_PATH = os.path.join("D:\\CodeAcademy\\c51_library_system\\background", "background.png")
+BACKGROUND_PATH = os.path.join("D:\CodeAcademy\\c51_library_system\\background", "background.png")
 
 def set_background(root, canvas_width=1400, canvas_height=800):
+    if not os.path.exists(BACKGROUND_PATH):
+        raise FileNotFoundError(f"Fono paveikslėlis nerastas: {BACKGROUND_PATH}")
     original_image = Image.open(BACKGROUND_PATH)
     resized_image = original_image.resize((canvas_width, canvas_height), Image.Resampling.LANCZOS)
     background_photo = ImageTk.PhotoImage(resized_image)
